6a5ae2ca64f96aae0db6e0a79090fa7b13fdb3b5
Chris G <59775715+headphone2005@users.noreply.github.com>
2023-02-25 13:43:26 -0500

    Update ArmSubsystem.java

--- src/main/java/frc/robot/Arm/ArmSubsystem.java
+++ src/main/java/frc/robot/Arm/ArmSubsystem.java
@@ -7,9 +7,12 @@ import com.revrobotics.SparkMaxPIDController;
 import com.revrobotics.CANSparkMax.ControlType;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import com.revrobotics.SparkMaxAlternateEncoder.Type;
-
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Robot;
+import frc.robot.Auto.SystemsCheck.SubChecker;
+import frc.robot.Auto.SystemsCheck.SystemsCheck;
 import frc.robot.Constants.ArmConstants;
 
 /*
@@ -30,7 +33,7 @@ import frc.robot.Constants.ArmConstants;
  * ⣿⣿⣿⣿⣶⣿⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
  */
 
-public class ArmSubsystem extends SubsystemBase {
+public class ArmSubsystem extends SubsystemBase implements SubChecker {
 
     public CANSparkMax arm;
 
@@ -49,10 +52,10 @@ public class ArmSubsystem extends SubsystemBase {
 
         neoEncoder = arm.getEncoder();
         throughBoreEncoder = arm.getAlternateEncoder(8192);
-        // absoluteEncoder = arm.getAbsoluteEncoder(com.revrobotics.SparkMaxAbsoluteEncoder.Type.kDutyCycle);
+        absoluteEncoder = arm.getAbsoluteEncoder(com.revrobotics.SparkMaxAbsoluteEncoder.Type.kDutyCycle);
 
         neoEncoder.setPositionConversionFactor(2*Math.PI / ArmConstants.GEARBOX_RATIO);
-        if (inStartingPos) neoEncoder.setPosition(ArmConstants.STARTING_ANGLE);
+        if (inStartingPos) neoEncoder.setPosition(2*Math.PI*absoluteEncoder.getPosition());
 
         controller = arm.getPIDController();
 
@@ -79,6 +82,25 @@ public class ArmSubsystem extends SubsystemBase {
             return;
         }
         controller.setReference(a, ControlType.kPosition);
+    }
+
+    public void reset() {
+        setAngle(0);
+    } 
+
+    public Command check(boolean safe) {
+        if (!safe) return new InstantCommand();
+        return new InstantCommand(() -> {
+            double start = System.currentTimeMillis();
+            boolean works = false;
+            setAngle(0.2);
+            while (System.currentTimeMillis() - start < 2000) {}
+            if (neoEncoder.getPosition() > 0.05) {
+                works = true;
+            }
+            SystemsCheck.setSystemStatus(this, works);
+            reset();
+            }, this);
     }
 
 }
